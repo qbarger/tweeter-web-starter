ca9a0e8591d4980fa4b5c4ea35cce44b147a6ee5
Quenton Barger <qbarger22@gmail.com>
2024-11-06 19:00:50 -0700

    GetFollowee lambda created

---
+++ tweeter-shared/src/model/net/request/PagedUserItemRequest.ts
@@ -0,0 +1,9 @@
+import { UserDto } from "../../dto/UserDto";
+
+export interface PagedUserItemRequest {
+  readonly token: string;
+  readonly userAlias: string;
+  readonly pageSize: number;
+  readonly lastItem: UserDto | null;
+}
+export default PagedUserItemRequest;
