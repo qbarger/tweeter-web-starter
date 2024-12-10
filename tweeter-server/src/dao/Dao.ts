ccc34ebed3591f259709a3a6937f3e17a079592d
Quenton Barger <qbarger22@gmail.com>
2024-12-09 19:15:56 -0700

    optional methods in Dao

--- tweeter-server/src/dao/Dao.ts
+++ tweeter-server/src/dao/Dao.ts
@@ -1,7 +1,7 @@
 export interface Dao<T> {
-  delete(request: T): Promise<void>;
-  put(request: T, input?: string): Promise<void>;
-  get(request: T): Promise<[T | undefined, string]>;
-  update(request: T): Promise<void>;
-  upload(fileName: string, imageStringBase64Encoded: string): Promise<string>;
+  delete?(request: T): Promise<void>;
+  put?(request: T, input?: string): Promise<void>;
+  get?(request: T): Promise<[T | undefined, string]>;
+  update?(request: T): Promise<void>;
+  upload?(fileName: string, imageStringBase64Encoded: string): Promise<string>;
 }
